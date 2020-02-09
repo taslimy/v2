27e20f499768cbb4f3be644a92b0eafb0d853ca1
taslimy <taslimer@gmail.com>
2020-02-09 17:35:14 -0500

    setting up data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from "react";
 import "./App.css";
 
+import projects from "./data/resume";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <div>
+          <h2>Taslim Yakub</h2>
+          <p>
+            Designer and Full-stack developer from Queens, New York. Currently
+            looking for{" "}
+            <a
+              href="https://www.linkedin.com/in/taslim-yakub-244070186/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Opportunities{" "}
+            </a>
+            as a designer or developer.
+          </p>
+          {projects.projects.items.map(r => (
+            <div key={r.id}>
+              <h1>{r.name}</h1>
+              <ul>
+                {r.bulletpoints.map(x => (
+                  <li>{x}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </header>
     </div>
   );
 }
---
+++ src/data/resume.js
@@ -0,0 +1,34 @@
+const projects = {
+  projects: {
+    id: 1,
+    title: "projects",
+    items: [
+      {
+        id: 1,
+        name: "Your Spotify",
+        description:
+          "My Spotify Dashboard is a Full-Stack React application that leverages Spotify's API to display information about the user's listening habits.",
+        bulletpoints: [
+          "Detailed visual of top artists & top tracks displayed on cards",
+          "List view of recently played tracks with live updates",
+          " Displays users following, follower count, & subscription status",
+          "The backend is using NodeJS and the front-end is using OAuth for authentication"
+        ]
+      },
+      {
+        id: 2,
+        name: "key conservation",
+        description:
+          "Key Conservation is a mobile application helping conservationists gain funding and global support by posting campaigns. A team of six developers and two UX/UI designers built the first version of this app over eight weeks. It is a legacy project that's apart of the Lambda School Labs program. The photos reflect the latest iteration of the app done by a new dev team.",
+        bulletpoints: [
+          "Pair programmed the front-end layout, navigation, screens & buttons",
+          "Pair programmed the backend using NodeJS & Express",
+          "Assisted in deployment of the front-end & back-end",
+          "Developed the app to be pixel perfect to match the desired look of our UX/UI designers & stakeholder"
+        ]
+      }
+    ]
+  }
+};
+
+export default projects;
