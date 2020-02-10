0ee2ceed0132906e746ede26128c9a9d08746d56
taslimy <taslimer@gmail.com>
2020-02-10 18:30:02 -0500

    basic template set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Sidebar from "./components/sidebar.component";
+import Projects from "./components/projects.component";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Projects />
     </>
   );
 }
--- src/components/projects.component.jsx
+++ src/components/projects.component.jsx
@@ -1,22 +1,131 @@
 import React from "react";
 
-import projects from "./data/resume";
+import projects from "../data/resume";
 
 import "./projects.styles.scss";
 
 const Projects = () => {
   return (
-    <div>
-      {projects.projects.items.map(r => (
-        <div key={r.id}>
-          <h1>{r.name}</h1>
-          <ul>
-            {r.bulletpoints.map(x => (
-              <li>{x}</li>
-            ))}
-          </ul>
+    <div class="project-wrapper">
+      <div
+        class="projects"
+        style={{
+          backgroundImage:
+            "-webkit-gradient(linear, left top, left bottom, from(#f4b93d), to(#f4b93d))",
+          backgroundImage: "linear-gradient(180deg, #f4b93d, #f4b93d)"
+        }}
+      >
+        <h4>01</h4>
+        <div class="wrap">
+          <img
+            class="image"
+            src="https://images.unsplash.com/photo-1542837201-998d8469d90a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=850"
+            alt="placeholder"
+          />
+          <img
+            class="shadow"
+            src="https://images.unsplash.com/photo-1542837201-998d8469d90a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=850"
+            alt="placeholder"
+          />
         </div>
-      ))}
+        <div class="project-heading">
+          <div class="project-information">
+            <h4>Project 01</h4>
+            <h5>Lorem ipsum letor exude</h5>
+          </div>
+          <div class="project-links">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Demo
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="projects"
+        style={{
+          backgroundImage:
+            "-webkit-gradient(linear, left top, left bottom, from(#005ba9), to(#005ba9))",
+          backgroundImage: "linear-gradient(180deg, #005ba9, #005ba9)"
+        }}
+      >
+        <h4>02</h4>
+        <div class="wrap">
+          <img
+            class="image"
+            src="https://images.unsplash.com/photo-1544961371-516024f8e267?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=850"
+            alt="placeholder"
+          />
+          <img
+            class="shadow"
+            src="https://images.unsplash.com/photo-1544961371-516024f8e267?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=850"
+            alt="placeholder"
+          />
+        </div>
+        <div class="project-heading">
+          <div class="project-information">
+            <h4>Project 02</h4>
+            <h5>Lorem ipsum letor exude</h5>
+          </div>
+          <div class="project-links">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Demo
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="projects"
+        style={{
+          backgroundImage:
+            "-webkit-gradient(linear, left top, left bottom, from(#b4ddbf), to(#b4ddbf))",
+          backgroundImage: "linear-gradient(180deg, #b4ddbf, #b4ddbf)"
+        }}
+      >
+        <h4>03</h4>
+        <div class="wrap">
+          <img
+            class="image"
+            src="https://images.unsplash.com/photo-1524726240783-939bfdd633e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=850"
+            alt="placeholder"
+          />
+          <img
+            class="shadow"
+            src="https://images.unsplash.com/photo-1524726240783-939bfdd633e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=850"
+            alt="placeholder"
+          />
+        </div>
+        <div class="project-heading">
+          <div class="project-information">
+            <h4>Project 03</h4>
+            <h5>Lorem ipsum letor exude</h5>
+          </div>
+          <div class="project-links">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Demo
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default Projects;
