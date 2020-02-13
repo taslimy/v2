f9f435d0a54dd542d4207a681ec0df1102a4079f
taslimy <taslimer@gmail.com>
2020-02-13 14:21:16 -0500

    added bubble animation fixed z-index issue

--- src/components/sidebar.component.jsx
+++ src/components/sidebar.component.jsx
@@ -4,22 +4,30 @@ import "./sidebar.styles.scss";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar-wrapper">
-      <h2>Taslim Yakub</h2>
-      <p>
-        Designer and Full-stack developer from Queens, New York. Currently
-        looking for
-        <a
-          href="https://www.linkedin.com/in/taslim-yakub-244070186/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Opportunities
-        </a>{" "}
-        as a designer or developer.
-      </p>
-    </div>
+    <>
+      <div className="sidebar-wrapper">
+        <div className="about-me">
+          <h2>Taslim Yakub</h2>
+          <p>
+            Designer and Full-stack developer from Queens, New York. Currently
+            looking for
+            <a
+              href="https://www.linkedin.com/in/taslim-yakub-244070186/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Opportunities
+            </a>{" "}
+            as a designer or developer.
+          </p>
+        </div>
+      </div>
+      <div className="bubble-wrapper">
+        <div className="square square-1"></div>
+        <div className="square square-2"></div>
+      </div>
+    </>
   );
 };
 
